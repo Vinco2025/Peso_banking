9802bd381bc99bef5b202d00cfb0a1827296fdb7
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-14 13:03:52 +0800

    added role and redirection for user and admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,13 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $role = auth()->user()->role;
+
+        return redirect(match($role) {
+            'admin' => route('admin.dashboard'),
+            'customer' => route('customer.dashboard'),
+            default => '/',
+        });
     }
 
     /**
