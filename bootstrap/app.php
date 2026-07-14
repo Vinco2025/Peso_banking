2508f69a8909ceb97190d4170297daa2724aa404
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-14 13:02:16 +0800

    added middleware for role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -10,8 +10,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'role' => \App\Http\Middleware\EnsureUserRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
