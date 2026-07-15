fa8a52358ed9135e8ff99824d64aeef8c45ce2d3
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-15 10:33:10 +0800

    added account models

---
+++ app/Models/Account.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'account_number',
+        'balance',
+        'type',
+    ];
+
+    //Shows if the account belongs to the user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // The account has many transactions
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class, 'from_account_id')
+                    ->orWhere('to_account_id', $this->id)
+                    ->latest();
+    }
+}
