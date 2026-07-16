4356619af598e522bd52d94389a1a8f534137143
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-16 23:05:37 +0800

    added history button

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -42,6 +42,10 @@
                     class="bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600">
                         Transfer
                 </a>
+                <a href="{{ route('transaction.history') }}"
+                class="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700">
+                    History
+                </a>
             </div>
 
         </div>
