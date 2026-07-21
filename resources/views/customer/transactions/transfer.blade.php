72ba53f1ceee1a2a445ee922a50c4d16a111b1d0
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-21 21:17:41 +0800

    change change the hue into green to match the design

--- resources/views/customer/transactions/transfer.blade.php
+++ resources/views/customer/transactions/transfer.blade.php
@@ -49,7 +49,7 @@
 
                     {{-- This button opens the modal instead of submitting directly --}}
                     <button type="button" onclick="openModal()"
-                        class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700">
+                        class="w-full bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-700">
                         Transfer
                     </button>
                 </form>
