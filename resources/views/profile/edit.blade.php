d95c82f719130a706d02886e3af34d39bf26f33f
Kevin Bartolome <liezelbart142029@gmail.com>
2026-07-22 17:15:50 +0800

    change the layout and design

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,29 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+        <div>
+            <h2 class="text-2xl font-bold text-gray-900">Profile Settings</h2>
+            <p class="text-sm text-gray-500 mt-1">Manage your personal information and security</p>
+        </div>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+    <div class="py-10">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            {{-- Profile Information --}}
+            <div class="bg-white border border-gray-100 rounded-2xl shadow-sm p-8">
+                @include('profile.partials.update-profile-information-form')
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+            {{-- Update Password --}}
+            <div class="bg-white border border-gray-100 rounded-2xl shadow-sm p-8">
+                @include('profile.partials.update-password-form')
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+            {{-- Delete Account --}}
+            <div class="bg-white border border-red-100 rounded-2xl shadow-sm p-8">
+                @include('profile.partials.delete-user-form')
             </div>
+
         </div>
     </div>
 </x-app-layout>
